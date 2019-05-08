270391468dc75a3ff32f886a381cbe288a14c849
pishi <pishi@pishis-MacBook.local>
2019-05-08 19:04:21 -0400

    add new functions to last ver

--- My Note- d3/my_note.js
+++ My Note- d3/my_note.js
@@ -1,10 +1,3 @@
-
-
-d3.select("h1").on("click", function(){
-	console.log("it is cliked");
-})
-
-
 d3.select("#new-note").on("submit", function(){
 	d3.event.preventDefault();
 	var input = d3.select("input");
@@ -14,4 +7,33 @@ d3.select("#new-note").on("submit", function(){
 	.classed("note", true)
 	.text(input.property("value"));
 	input.property("value","");
+	setPreview("");
 })
+
+d3.select(".remove").on("click", function(){
+	d3.event.preventDefault();
+	d3.selectAll("p").remove();
+})
+
+
+var preview = d3.select(".preview");
+var input = d3.select("input");
+
+d3.select(".lucky").on("click", function(){
+	d3.event.preventDefault();
+	d3.selectAll("p").style("font-size", function(){
+		return Math.random() * 40 + "px";
+	})
+})
+
+
+input.on("input", function(){
+	var note = d3.event.target.value;
+	setPreview(note);
+})
+
+function setPreview(val){
+	preview.text(val)
+	.classed("hide", val === "");
+}
+
